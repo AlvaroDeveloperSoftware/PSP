8fe9a1978919e63200b9947c5c35f3413f06164c
alvaroramasfranco <alvaroramasfranco@gmail.com>
2021-10-13 21:37:25 +0200

    Ejemplo de c

---
+++ ejemplo1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	printf("Llamando a varios comandos desde la función system()\n");
+    printf("Contenido del directorio actual\n");
+    system("ls -l");
+    printf("Enviando el contenido del directorio actual a un fichero de texto \n");
+    system("ls -l > salida.txt");
+    printf("Se ha creado el fichero salida.txt\n");
+    printf("Se abre el fichero salida.txt con gedit\n");
+    //Se añade a system para abrir el fichero de texto
+    //con la salida del comando ls
+    system("gedit salida.txt");
+    printf("\nProbando un comando erróneo: %d", 
+    system("cop"));
+    printf("\nPrograma finalizado\n");
+}
